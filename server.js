d5ec6ed80f8aa3923b3a4bc749e6c5456b1f1368
ramiksaier <ramos.ksaier@gmail.com>
2021-05-05 22:25:03 +0100

    thirde add deployment

--- server.js
+++ server.js
@@ -14,12 +14,11 @@ app.use("/api/docteur", require("./Router/Docteur"));
 app.use("/api/rendezvous", require("./Router/Rendevous"));
 app.use("/api/admin", require("./Router/Admin"));
 
-if(process.env.NODE_ENV==='production'){
-  app.use(express.static('client/build'));
-  app.get('*',(req,res)=>{
-
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 //connect to server
